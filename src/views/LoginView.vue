8788b33449d75940773ebcf64f9705249f15501f
Avaz <elonarm21@gmail.com>
2023-03-10 11:18:37 +0500

    routing

---
+++ src/views/LoginView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="login">
+    <h2>login page</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoginView",
+};
+</script>
+
+<style></style>
---
+++ src/views/RegisterView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="register">
+    <h2>Register page</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RegisterView",
+};
+</script>
+
+<style></style>
